756ab4e7e2adc2ddf34f399c3740ab3a57dacfdc
Christian <christian.rulloda@gmail.com>
2024-02-17 20:50:08 +0800

    Passing Data To Views hobbies.blade.php

---
+++ resources/views/hobbies.blade.php
@@ -0,0 +1,7 @@
+<hr>
+<h3>Hobbies</h3>
+<ul>
+    <li>Driving: I love the freedom and thrill of exploring new places behind the wheel, whether it's cruising along scenic routes or embarking on road trips with friends.</li>
+    <li>Playing Video Games: Immersing myself in virtual worlds, solving puzzles, and competing with friends online brings me endless enjoyment and challenges my strategic thinking..</li>
+    <li>Watching Movies: There's nothing quite like losing myself in captivating stories on the big screen, from gripping dramas that evoke deep emotions to action-packed adventures that keep me on the edge of my seat.</li>
+</ul>
