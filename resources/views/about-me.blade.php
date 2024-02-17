f1f75f1af600e0e0f3fdd374b365915e879854ec
Christian <christian.rulloda@gmail.com>
2024-02-17 20:47:08 +0800

    Passing Data To Views about-me.blade.php

---
+++ resources/views/about-me.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Me</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1 class="mt-4">About Me</h1>
+        <p class="lead">Welcome to my About Me page!</p>
+        <hr>
+        <h3>Personal Information</h3>
+        <ul>
+            <li>Name: Christian Errol Miguel S.</li>
+            <li>Age: 23</li>
+            <li>Location: Hillside, Baguio City</li>
+        </ul>
+</body>
+</html>
